1d453b685c7a8572f1b3ee1f6e0263d3e48685d1
bucky <bucky123@gmail.com>
2018-08-24 13:21:41 +0545

    delete connection.php

--- webexample/connection.php
+++
@@ -1,6 +0,0 @@
-<?php
-$error = 'Error has been occured';
-$con = mysqli_connect('localhost','root',''); //connection to the mysql server
-mysqli_select_db($con,'Web') or die ($error); //select the particular database
-
-?>
